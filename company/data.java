da539c83855e8efef197fcdedd49af7e0f3d660c
Sanzhar357 <80056086+Sanzhar357@users.noreply.github.com>
2021-03-10 02:09:15 +0600

    Delete data.java

--- company/data.java
+++
@@ -1,8 +0,0 @@
-private static void EnsureDirectoryExists(string filePath) 
-{ 
-  FileInfo fi = new FileInfo(filePath);
-  if (!fi.Directory.Exists) 
-  { 
-    System.IO.Directory.CreateDirectory(fi.DirectoryName); 
-  } 
-}
