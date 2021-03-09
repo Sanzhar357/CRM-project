afb512d09964ef73bee1659b3df9d5683b2b80ae
Sanzhar357 <80056086+Sanzhar357@users.noreply.github.com>
2021-03-10 02:31:11 +0600

    Create IProductController.java

---
+++ company/controllers/interfaces/IProductController.java
@@ -0,0 +1,12 @@
+package com.company.controllers.interfaces;
+
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+public interface IProductController {
+    public void ItemLoop();
+
+    public void letterOfNameDescrip();
+
+    public void massOfProduct();
+}
