06e2decf1f5612c3defcaaf034793e4ebb6f854c
Sanzhar357 <80056086+Sanzhar357@users.noreply.github.com>
2021-03-10 02:28:48 +0600

    Create ICustomerInputController.java

---
+++ company/controllers/interfaces/ICustomerInputController.java
@@ -0,0 +1,12 @@
+package com.company.controllers.interfaces;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public interface ICustomerInputController  {
+    public void InputData();
+}
